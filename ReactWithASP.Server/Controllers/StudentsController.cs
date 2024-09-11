baf8d5840d0210dbaed1b115603e9d0e1dc7a340
austy <austy@MSI>
2024-09-11 14:20:52 +0300

    Add project files.

---
+++ ReactWithASP.Server/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactWithASP.Server.Models.DTOs;
+using ReactWithASP.Server.Models.Data;
+namespace ReactWithASP.Server.Controllers;
+
+[ApiController]
+[Route(template:"api/[controller]")]
+
+public class StudentsController(AppDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var students = await context.Students.ToListAsync();
+        List<StudentDto> results = [];
+
+        foreach (var student in students)
+        {
+            results.Add(new StudentDto(student.Id, FullName:$"{student.FirstName} {student.LastName}", student.Email));
+        }
+        return Ok(results);
+    }
+}
---
+++ ReactWithASP.Server/Models/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactWithASP.Server.Models.Entities;
+public class Student(string firstName, string lastName, string email) : Entity<int>
+{
+    [MaxLength(30)] public string FirstName { get; protected set; } = firstName;
+
+    [MaxLength(30)] public string LastName { get; protected set; } = lastName;
+
+    [MaxLength(30)] public string Email { get; protected set; } = email;
+
+}
+
