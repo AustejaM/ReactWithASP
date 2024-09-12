f2e66856090b975d14bd1113bf36c31b6328d5ca
Austeja <austeja.malinauskaite@panko.lt>
2024-09-12 22:29:19 +0300

    3_darbas

--- ReactWithASP.Server/Controllers/StudentsController.cs
+++ ReactWithASP.Server/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using ReactWithASP.Server.Models.DTOs;
 using ReactWithASP.Server.Models.Data;
+using ReactWithASP.Server.Models.Entities;
 namespace ReactWithASP.Server.Controllers;
 
 [ApiController]
@@ -14,9 +15,24 @@ public class StudentsController(AppDbContext context) : ControllerBase
     {
         var students = await context.Students.ToListAsync();
         List<StudentDto> results = [];
-        foreach (var student in students) { 
-            results.Add(new StudentDto(student.Id,student.FirstName,student.LastName, student.Email));
+        foreach (var student in students)
+        {
+            results.Add(new StudentDto(student.Id, student.FirstName, student.LastName, student.Email));
         }
         return Ok(results);
+    }
+
+    [HttpPut(template: "{id:int}")]
+
+    public async Task<IActionResult> Put(int id, StudentDto dto)
+    {
+        var student = await context.Students.FirstOrDefaultAsync(i => i.Id == id);
+        if (student != null)
+        {
+            student.SetValues(dto.FirstName, dto.LastName, dto.Email);
+            context.Students.Update(student);
+            await context.SaveChangesAsync();
+        }
+        return Ok();
     }
 }
